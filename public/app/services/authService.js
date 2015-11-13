9f518ab774c28c6c5a56d04f8daab8d765bde6ed
jonandthink <jonandthink@gmail.com>
2015-11-13 16:19:21 +0700

    Working. Still checking bugs

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -1,9 +1,15 @@
-angular.module('authService',[])
+angular.module('authService', [])
 
-.factory('Auth',function($http, $q, AuthToken)){
+
+
+.factory('Auth', function($http, $q, AuthToken) {
+
+
 	var authFactory = {};
 
+
 	authFactory.login = function(username, password) {
+
 		return $http.post('/api/login', {
 			username: username,
 			password: password
@@ -18,17 +24,72 @@ angular.module('authService',[])
 		AuthToken.setToken();
 	}
 
-	authFactory.isloggedIn = function() {
+	authFactory.isLoggedIn = function() {
 		if(AuthToken.getToken())
-			return true;
+				return true;
 		else
 			return false;
 	}
 
 	authFactory.getUser = function() {
-		if (AuthToken.getToken())
+		if(AuthToken.getToken())
 			return $http.get('/api/me');
 		else
-			return $q.reject({mesage:"User has no token"});
+			return $q.reject({ message: "User has no token"});
+
 	}
-}
+
+
+	return authFactory;
+
+})
+
+
+.factory('AuthToken', function($window) {
+
+	var authTokenFactory = {};
+
+	authTokenFactory.getToken = function() {
+		return $window.localStorage.getItem('token');
+	}
+
+	authTokenFactory.setToken = function(token) {
+
+		if(token)
+			$window.localStorage.setItem('token', token);
+		else
+			$window.localStorage.removeItem('token');
+
+	}
+
+	return authTokenFactory;
+
+})
+
+
+.factory('AuthInterceptor', function($q, $location, AuthToken) {
+
+	var interceptorFactory = {};
+
+
+	interceptorFactory.request = function(config) {
+
+		var token = AuthToken.getToken();
+
+		if(token) {
+
+			config.headers['x-access-token'] = token;
+
+		}
+
+		return config;
+
+	};
+
+	
+
+
+	return interceptorFactory;
+});
+
+
