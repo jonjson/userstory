cfebe4afa710bd78d69e2f16726241d36977e1f6
jonandthink <jonandthink@gmail.com>
2015-11-13 07:08:02 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"database": "mongodb://jonandthink:142735689@ds053794.mongolab.com:53794/jonandthink",
+	"port": process.env.PORT || 3000,
+	"secretKey": "JonisThinking"
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+
+var config = require('./config');
+
+var app = express();
+
+mongoose.connect(config.database, function(err) {
+	if(err){
+		console.log(err);
+	}else {
+		console.log('Connected to the database');
+	}
+});
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+var api = require('./app/routes/api')(app,express);
+app.use('/api',api);
+
+app.get('*', function(req,res) {
+	res.sendFile(__dirname + '/public/views/index.html');
+});
+
+app.listen(config.port, function(err){
+	if(err) {
+		console.log(err);
+	}else {
+		console.log("Listening to port 3000");
+	}
+});
